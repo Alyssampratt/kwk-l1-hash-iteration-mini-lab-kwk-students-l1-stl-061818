802bd27be263e56d1b5e91355a85f741c3064ab8
Alyssampratt <magoopratt@gmail.com>
2018-06-21 16:34:53 +0000

    automatically backed up by learn

---
+++ get_input.rb
@@ -0,0 +1,3 @@
+puts "Please input your birth month"
+birth_month = gets.chomp
+puts "oh thats great! birthdays in #{birth_month.capitalize} are the best"
--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -14,6 +14,26 @@ summer_olympics.each do |year, place|
 puts "The #{year} summer olympics took place in #{place}" 
 end 
 end
-
 create_olympics_hash
+
+def each_key
+  summer_olympics = {
+  :Sydney => 2000
+  :Athens => 2004
+  :Beijing => 2008
+  :London => 2012
+  :Atlanta => 1996}
+  
+  places = summer_olympics.keys
+  year = summer_olympics.values
+  
+  upcased_places=[]
+  summer_olympics.each do |places|
+    upcased_places.push(places.upcased_places)
+    
+    puts upcased_places
+  end
+  each_key
+  
+end
 
